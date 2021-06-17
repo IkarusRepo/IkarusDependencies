716f08230f71daf6b5264fac62d3eeb9c13785e7
AlexanderMueller <mueller@ibb.uni-stuttgart.de>
2021-06-17 14:05:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Ikarus_Dependencies
---
+++ installIkarusDependencies.sh
@@ -0,0 +1,16 @@
+mkdir cmake-build-debug
+cd cmake-build-debug
+
+cmake ../  -DCMAKE_BUILD_TYPE=Debug -G "CodeBlocks - MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND"
+
+cmake --build . --parallel 9
+
+cd ..
+mkdir cmake-build-release
+cd cmake-build-release
+
+cmake ../  -DCMAKE_BUILD_TYPE=Release -G "CodeBlocks - MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND"
+
+cmake --build . --parallel 9
+
+cmd /k
