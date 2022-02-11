d117e6dc79b092e06810e7a6f917d93ccd211c8f
AlexanderMueller <mueller@ibb.uni-stuttgart.de>
2022-02-11 08:37:34 +0100

    add patch files fordune-alugrid and dune-grid-glue

--- buildkarusDependencies.sh
+++ buildkarusDependencies.sh
@@ -4,7 +4,7 @@
 #cmake --build . --parallel 9
 #cd ..
 
-mkdir cmake-build-release
+# mkdir cmake-build-release
 cd cmake-build-release
 
 cmake ../  -DCMAKE_BUILD_TYPE=Release -G "CodeBlocks - MinGW Makefiles" -DCMAKE_SH="CMAKE_SH-NOTFOUND"
